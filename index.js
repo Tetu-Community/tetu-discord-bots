bfad9df16d380f5269839521fc04566d090f4a13
belbix <7453635@gmail.com>
2023-07-18 10:06:48 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const axios = require('axios')
-const delay = require('delay')
-const ms = require('ms')
-const { Client } = require('discord.js')
+const axios         = require('axios')
+const delay         = require('delay')
+const ms            = require('ms')
+const { Client }    = require('discord.js')
 const StatusUpdater = require('@tmware/status-rotate')
-const BigNumber = require('bignumber.js')
-const ethers = require('ethers')
-const reject = require('lodash.reject')
+const BigNumber     = require('bignumber.js')
+const ethers        = require('ethers')
+const reject        = require('lodash.reject')
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
@@ -13,11 +16,11 @@ const CONTRACT_READER_ADDRESSES = {
   polygon: '0xCa9C8Fba773caafe19E6140eC0A7a54d996030Da',
   fantom: '0xa4EB2E1284D9E30fb656Fe6b34c1680Ef5d4cBFC',
   bsc: '0xE8210A2d1a7B56115a47B8C06a72356773f6838E',
-  mainnet: '0x6E4D8CAc827B52E7E67Ae8f68531fafa36eaEf0B'
+  mainnet: '0x6E4D8CAc827B52E7E67Ae8f68531fafa36eaEf0B',
 }
 
 const IGNORED_VAULTS = [
-  '0x8A12335d417F5aa82D803769b34c5b9d990a623B'
+  '0x8A12335d417F5aa82D803769b34c5b9d990a623B',
 ]
 
 function truncateErr (err) {
@@ -26,7 +29,9 @@ function truncateErr (err) {
 
 function getProvider (network) {
   const urlStr = process.env[`${network.toUpperCase()}_RPC_HTTP`]
-  if (!urlStr) throw new Error(`no RPC provider for network ${network} found!`)
+  if (!urlStr) {
+    throw new Error(`no RPC provider for network ${network} found!`)
+  }
   return jsonRpcProviderFromUrl(urlStr)
 }
 
@@ -35,7 +40,7 @@ function jsonRpcProviderFromUrl (urlStr) {
   return new ethers.providers.JsonRpcProvider({
     url: u.origin + u.pathname,
     user: u.username,
-    password: u.password
+    password: u.password,
   })
 }
 
@@ -43,7 +48,7 @@ async function balancerPriceQuote ({ provider, poolId, inputToken, outputToken, 
   const contract = new ethers.Contract(
     '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
     require('./abis/BalancerVault.json'),
-    provider
+    provider,
   )
 
   const swap0 = {
@@ -51,19 +56,19 @@ async function balancerPriceQuote ({ provider, poolId, inputToken, outputToken, 
     assetInIndex: 0,
     assetOutIndex: 1,
     amount: inputAmount,
-    userData: '0x'
+    userData: '0x',
   }
 
   const assets = [
     inputToken,
-    outputToken
+    outputToken,
   ]
 
   const funds = {
     sender: ADDRESS_ZERO,
     fromInternalBalance: false,
     recipient: ADDRESS_ZERO,
-    toInternalBalance: false
+    toInternalBalance: false,
   }
 
   const quote = await contract.callStatic.queryBatchSwap(0, [swap0], assets, funds)
@@ -75,13 +80,13 @@ async function tetuSwapPriceQuote ({ provider, router, pair, fee, inputToken, ou
   const routerContract = new ethers.Contract(
     router,
     require('./abis/TetuSwapRouter.json'),
-    provider
+    provider,
   )
 
   const pairContract = new ethers.Contract(
     pair,
     require('./abis/UniswapV2Pair.json'),
-    provider
+    provider,
   )
 
   const reserves = await pairContract.getReserves()
@@ -92,14 +97,14 @@ async function tetuSwapPriceQuote ({ provider, router, pair, fee, inputToken, ou
     inputAmount,
     inputTokenIsReserveZero ? reserves[0] : reserves[1],
     inputTokenIsReserveZero ? reserves[1] : reserves[0],
-    fee
+    fee,
   )
 
   return res.toString()
 }
 
 async function newClientWithStatusUpdater (key) {
-  const bot = new Client({ intents: [] })
+  const bot         = new Client({ intents: [] })
   bot.statusUpdater = new StatusUpdater(bot)
   bot.login(key)
 
@@ -108,7 +113,9 @@ async function newClientWithStatusUpdater (key) {
   for (const guildId of process.env.GUILD_IDS.split(',')) {
     try {
       const guild = await bot.guilds.fetch(guildId)
-      if (!guild) throw new Error('cannot find guild with id', guildId)
+      if (!guild) {
+        throw new Error('cannot find guild with id', guildId)
+      }
       guilds.push(guild)
     } catch (err) {
       console.log('could not find guild', truncateErr(err))
@@ -123,7 +130,9 @@ async function updateStatus (bot, guilds, nickname, statusMsg) {
     statusMsg = statusMsg.substring(0, 128)
   }
 
-  if (process.env.VERBOSE) console.log('updating bot', nickname, statusMsg)
+  if (process.env.VERBOSE) {
+    console.log('updating bot', nickname, statusMsg)
+  }
 
   if (nickname) {
     for (const guild of guilds) {
@@ -132,7 +141,7 @@ async function updateStatus (bot, guilds, nickname, statusMsg) {
     }
   }
 
-  const s = { type: 'WATCHING', name: statusMsg }
+  const s = { type: 3, name: statusMsg }
   await bot.statusUpdater.addStatus(s)
   await bot.statusUpdater.updateStatus(s)
 }
@@ -148,7 +157,7 @@ async function runTetuPriceBot () {
         bot,
         guilds,
         `TETU $${resp.data.market_data.current_price.usd}`,
-        `24h: ${BigNumber(resp.data.market_data.price_change_percentage_24h_in_currency.usd).toFixed(1)}%`
+        `24h: ${BigNumber(resp.data.market_data.price_change_percentage_24h_in_currency.usd).toFixed(1)}%`,
       )
     } catch (err) {
       console.log('error in runTetuPriceBot', truncateErr(err))
@@ -170,7 +179,7 @@ async function runTetuCirculatingSupplyBot () {
         bot,
         guilds,
         BigNumber(resp.data).toFormat(2),
-        'Circulating Supply'
+        'Circulating Supply',
       )
     } catch (err) {
       console.log('error in runTetuCirculatingSupplyBot', truncateErr(err))
@@ -192,17 +201,17 @@ async function runTetuBalDiscountBot () {
         poolId: '0xb797adfb7b268faeaa90cadbfed464c76ee599cd0002000000000000000005ba',
         inputToken: '0x7fc9e0aa043787bfad28e29632ada302c790ce33',
         outputToken: '0x3d468ab2329f296e1b9d8476bb54dd77d8c2320f',
-        inputAmount: ethers.utils.parseEther('1')
+        inputAmount: ethers.utils.parseEther('1'),
       })
 
       const bptForTetuBal = BigNumber(resp.toString()).shiftedBy(-18)
-      const discount = BigNumber(1).minus(bptForTetuBal)
+      const discount      = BigNumber(1).minus(bptForTetuBal)
 
       await updateStatus(
         bot,
         guilds,
         `${bptForTetuBal.toFixed(4)} (${discount.times(100).toFixed(1)}%)`,
-        'tetuBAL discount'
+        'tetuBAL discount',
       )
     } catch (err) {
       console.log('error in runTetuBalDiscountBot', truncateErr(err))
@@ -224,17 +233,17 @@ async function runTetuQiDiscountBot () {
         poolId: '0xd80ef9fabfdc3b52e17f74c383cf88ee2efbf0b6000000000000000000000a65',
         inputToken: '0x4cd44ced63d9a6fef595f6ad3f7ced13fceac768',
         outputToken: '0x580a84c73811e1839f75d86d75d88cca0c241ff4',
-        inputAmount: ethers.utils.parseEther('1')
+        inputAmount: ethers.utils.parseEther('1'),
       })
 
       const qiForTetuQi = BigNumber(resp.toString()).shiftedBy(-18)
-      const discount = BigNumber(1).minus(qiForTetuQi)
+      const discount    = BigNumber(1).minus(qiForTetuQi)
 
       await updateStatus(
         bot,
         guilds,
         `${qiForTetuQi.toFixed(4)} (${discount.times(100).toFixed(1)}%)`,
-        'tetuQI discount'
+        'tetuQI discount',
       )
     } catch (err) {
       console.log('error in runTetuQiDiscountBot', truncateErr(err))
@@ -251,32 +260,20 @@ async function runTetuTvlBot () {
 
   async function loop () {
     try {
-      let tvl = BigNumber(0)
+      const resp = await axios.get('https://api.llama.fi/protocol/tetu')
 
-      for (const [chain, addr] of Object.entries(CONTRACT_READER_ADDRESSES)) {
-        const provider = getProvider(chain)
+      let tvl = 0;
 
-        const contractReader = new ethers.Contract(
-          addr,
-          require('./abis/TetuContractReader.json'),
-          provider
-        )
-
-        const vaults = await contractReader.vaults()
-
-        const resp = await contractReader.totalTvlUsdc(reject(vaults, v => IGNORED_VAULTS.includes(v)))
-
-        tvl = tvl.plus(resp.toString())
-      }
+      Object.values(resp.data.currentChainTvls).forEach(t => tvl += Number(t));
 
       await updateStatus(
         bot,
         guilds,
-        `$${BigNumber(tvl.toString()).shiftedBy(-18).toFormat(2)}`,
-        'Total Value Locked'
+        '$' + BigNumber(tvl).toFormat(0),
+        'TVL',
       )
     } catch (err) {
-      console.log('error in runTetuTvlBot', truncateErr(err))
+      console.log('error in runTetuCirculatingSupplyBot', truncateErr(err))
     }
 
     await delay(ms('5m'))
@@ -288,4 +285,4 @@ setTimeout(runTetuPriceBot, 0)
 setTimeout(runTetuCirculatingSupplyBot, 0)
 setTimeout(runTetuBalDiscountBot, 0)
 setTimeout(runTetuQiDiscountBot, 0)
-// setTimeout(runTetuTvlBot, 0)
+setTimeout(runTetuTvlBot, 0)
